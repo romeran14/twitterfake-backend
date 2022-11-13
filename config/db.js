6e7ae0e5c1769f20e6621efd590933d3a64547a9
Romeran Rodriguez <romeran14@gmail.com>
2022-11-13 17:00:16 -0400

    probando con PORT DB

--- config/db.js
+++ config/db.js
@@ -3,12 +3,14 @@ const HOST = process.env.HOST
 const USER_DB= process.env.USER_DB
 const PASSWORD_DB = process.env.PASSWORD_DB
 const DATA_BASE = process.env.DATA_BASE
+const DATA_BASE_PORT =process.env.DATA_BASE_PORT
 const db = mysql.createConnection({
   host     : HOST,
   user     : USER_DB,
   password : PASSWORD_DB,
   database : DATA_BASE,
-  multipleStatements: true
+  multipleStatements: true,
+  port : DATA_BASE_PORT
 });
 
 module.exports = db;
