ac2b3c9a1ac77c9d62e5c621ca3fec40e8c882e3
romeran14 <romeran14@gmail.com>
2025-08-04 01:54:58 +0200

    ggg

--- config/db.js
+++ config/db.js
@@ -5,25 +5,28 @@ const USER_DB= process.env.USER_DB
 const PASSWORD_DB = process.env.PASSWORD_DB
 const DATA_BASE = process.env.DATA_BASE
 const DATA_BASE_PORT =process.env.DATA_BASE_PORT
+// const db = mysql.createConnection({
+//   multipleStatements: true,
+//   uri:process.env.DATABASE_URL,
+//       waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// })
+
 const db = mysql.createConnection({
+  host     : HOST,
+  user     : USER_DB,
+  password : PASSWORD_DB,
+  
+  database : DATA_BASE,
   multipleStatements: true,
-  uri:process.env.DATABASE_URL,
-      waitForConnections: true,
+  port : DATA_BASE_PORT,
+  ssl:{
+    rejectUnauthorized:false
+  },
+        waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-})
-
-// const db = mysql.createConnection({
-//   host     : HOST,
-//   user     : USER_DB,
-//   password : PASSWORD_DB,
-  
-//   database : DATA_BASE,
-//   multipleStatements: true,
-//   port : DATA_BASE_PORT,
-//   ssl:{
-//     rejectUnauthorized:false
-//   }
-// });
+});
 console.log("DATABASEEEEEE",  HOST, USER_DB,PASSWORD_DB, DATA_BASE, DATA_BASE_PORT,db.authorized)
 module.exports = db;
